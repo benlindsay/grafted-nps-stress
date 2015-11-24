1d8937e0c8d380346f4cd2bed3b26cf12adc786f
ben-lindsay <benjlindsay@gmail.com>
2015-11-23 22:48:31 -0500

    cleaned up homopolymer_discrete.cpp

--- homopolymer_discrete.cpp
+++ homopolymer_discrete.cpp
@@ -1,49 +1,39 @@
 #include "globals.h"
 
-
 complex<double> homopolymer_discrete( complex<double> *WA,  
     complex<double> **q , 
     int N   ) {
 
   int i,n,j,k, nn[Dim];
-
   complex<double> *qf, *h, *a, *w ;
-  int Nflip[Dim];
-  for (i=0; i<Dim; i++) 
-    Nflip[Dim-i-1] = Nx[i];
-
   double temp;
   double arg, indx, indy, indz, kvec[Dim];
+  int Nflip[Dim];
 
+  for (i=0; i<Dim; i++) 
+    Nflip[Dim-i-1] = Nx[i];
 
   fftw_plan qplan, hplan;
 
   qf = (complex<double>*) fftw_malloc(sizeof(complex<double>)*size);
   h =  (complex<double>*) fftw_malloc(sizeof(complex<double>)*size);
   a =  (complex<double>*) fftw_malloc(sizeof(complex<double>)*size);
- 
 
   // Initial condition on q(s=0, x)
   for (i=0; i<ML; i++) 
       q[0][i] = exp( -WA[i] );
 
-
   for (n=1; n<N; n++) {
 
     ///////////////////////////
-    // ForWArd propagator, q //
+    // Forward propagator, q //
     ///////////////////////////
-
     
     // Go to k-space to evaluate the convolution //
     fft_fwd_wrapper(q[n-1], a);
 
-    for (j=0; j<ML; j++) {
-//      temp = get_k(j, kvec);
-//      h[j] = a[j] * exp(-temp);
-
+    for (j=0; j<ML; j++)
       h[j] = a[j] * poly_bond_fft[j] ;
-    }
 
     // IFFT h to get q^{n+1/2}
     fft_bck_wrapper(h, qf);
@@ -52,13 +42,7 @@ complex<double> homopolymer_discrete( complex<double> *WA,
     for (j=0; j<ML; j++)
       q[n][j] = qf[j] * exp( -WA[j] );
 
-
-
-
-
-
-  }//for (n=0; n<N...
-
+  } //for (n=0; n<N...
 
   complex<double> Q = integ_trapPBC(q[N-1]) / V;
   
